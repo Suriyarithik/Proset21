2157f34a538174083e3e45fee3fdb5f4f4c916a2
Suriyarithik <suriyavedsum@gmail.com>
2017-07-15 11:40:38 +0530

    Create Proset21.java

---
+++ Proset21.java
@@ -0,0 +1,32 @@
+# Proset21
+import java.util.Scanner;
+public class Proset21 {
+  public static void main(String[] args) {
+    Scanner s=new Scanner(System.in);
+    System.out.println("Enter Number of elements in array");
+    int n=s.nextInt();
+    int a[]=new int[n];
+    int mid=n/2;
+    int sum1=0;
+    int sum2=0;
+    float mean1;
+    float mean2;
+    System.out.println("Enter Values");
+    for(int i=0;i<n;i++){
+      a[i]=s.nextInt();
+      
+      if(i>=mid)
+      sum1+=a[i];
+      else
+        sum2+=a[i];
+    }
+ 
+mean1=sum2/mid;
+mean2=sum1/(n-mid);
+if(mean1==mean2)
+  System.out.println("Possible");
+else
+  System.out.println("Not Possible");
+s.close();}
+  
+}
